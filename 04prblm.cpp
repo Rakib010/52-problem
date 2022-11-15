49074b38ebe58a74e180d74aa55935b25b588c85
Rakib010 <112962930+Rakib010@users.noreply.github.com>
2022-11-15 23:54:49 +0600

    Add files via upload

--- 04prblm.cpp
+++ 04prblm.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 int main()
 {
-    int t;
+    int t,n;
     cin>>t;
-    while(t--)
-    {
-        int n,r;
+  for(t=1;t<=3;t++)
+     {
         cin>>n;
+        cout<<"case "<<t<<":";
+
         for(int i=1; i<=n; i++)
         {
-
             if(n%i==0)
-            cout<<i<<" ";
+            {
+                cout<<i<<" ";
+            }
 
         }
+
     }
 
 }
